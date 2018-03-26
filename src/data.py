e82fb110c03129e99ac5dad4e6666213b12988d4
Daniel Kramer <drkspace@gmail.com>
2018-03-26 13:27:17 -0700

    some work done

--- src/data.py
+++ src/data.py
@@ -1,5 +1,8 @@
 import nflgame as ng
+import teams
+import pprint
 
+teams = teams.teams()
 
 year = 2017
 
@@ -7,5 +10,13 @@ data = [[[], [], [], []] for _ in range(32)]
 
 for week in range(1,18):
     games = ng.games(year, week)
-    for i in games:
-
+    plays = ng.combine_plays(games)
+    for p in plays:
+        tNum = teams.teamNumber(p.team)
+        if p.down not in [1,2,3,4]:
+            continue
+        data = p.data
+        #pprint.pprint(data)
+        print data
+        raw_input()
+       
